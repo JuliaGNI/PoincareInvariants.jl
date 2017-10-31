26757744b1fbb4f3d869da06ad0eb90f67aa2594
Michael Kraus <michael.kraus@ipp.mpg.de>
2017-10-31 15:21:26 +0100

    Initial Commit. Extracted Poincaré invariant diagnostic from GeometricIntegrators.jl.

---
+++ src/PoincareInvariants.jl
@@ -0,0 +1,24 @@
+__precompile__()
+
+module PoincareInvariants
+
+    using ApproxFun
+    using HDF5
+    using ProgressMeter
+    using GeometricIntegrators
+    using GeometricIntegrators.Utils
+
+    export PoincareInvariant1st, PoincareInvariant1stCanonical,
+           PoincareInvariant2nd, PoincareInvariant2ndCanonical,
+           PoincareInvariant2ndTrapezoidal,
+           evaluate_poincare_invariant, write_to_hdf5
+
+    include("poincare_invariant_1st_common.jl")
+    include("poincare_invariant_1st.jl")
+    include("poincare_invariant_1st_canonical.jl")
+    include("poincare_invariant_2nd.jl")
+    include("poincare_invariant_2nd_canonical.jl")
+    include("poincare_invariant_2nd_trapezoidal.jl")
+    include("poincare_invariant_2nd_common.jl")
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,9 @@
+
+using PoincareInvariants
+using GeometricIntegrators
+using GeometricIntegrators.Utils
+using Base.Test
+
+include("poincare_invariant_1st_tests.jl")
+include("poincare_invariant_2nd_unittests.jl")
+include("poincare_invariant_2nd_tests.jl")
