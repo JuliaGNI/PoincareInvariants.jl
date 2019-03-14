607224263ac01e7fafc8dfdfaa905cf7e1c98fef
Michael Kraus <michael.kraus@ipp.mpg.de>
2019-03-14 09:51:46 +0100

    Update tests and Travis config for Julia 1.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@
 using PoincareInvariants
 using GeometricIntegrators
 using GeometricIntegrators.Utils
-using Base.Test
+using Test
 
 include("poincare_invariant_1st_tests.jl")
 include("poincare_invariant_2nd_unittests.jl")
