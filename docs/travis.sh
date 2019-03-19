57199d4cb0eede844f4e154ef7b6483aa3a1ab2e
Michael Kraus <michael.kraus@ipp.mpg.de>
2019-03-19 16:10:12 +0100

    Update docs deployment.

--- docs/travis.sh
+++ docs/travis.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 if [ ${TRAVIS_OS_NAME} = "linux" ]; then
-    julia --color=yes -e 'Pkg.add("Documenter")';
-    julia --color=yes -e 'cd(Pkg.dir("PoincareInvariants")); include(joinpath("docs", "make.jl"))';
+    julia --color=yes -e 'using Pkg; Pkg.add("Documenter"); Pkg.instantiate(); import PoincareInvariants; include(joinpath(dirname(pathof(PoincareInvariants)), "..", "docs", "make.jl"))';
 fi
