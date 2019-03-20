ccb191a93aea982f70fcfe53368995274987207a
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-03-20 12:49:26 +0000

    build based on 57199d4

---
+++ v0.1.0/search_index.js
@@ -0,0 +1,75 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#PoincareInvariants.jl-1",
+    "page": "Home",
+    "title": "PoincareInvariants.jl",
+    "category": "section",
+    "text": "Julia library for the computation of Poincaré integral invariants.(Image: Build Status) (Image: Coverage Status) (Image: codecov)"
+},
+
+{
+    "location": "#License-1",
+    "page": "Home",
+    "title": "License",
+    "category": "section",
+    "text": "Copyright (c) 2017 Michael Kraus <michael.kraus@ipp.mpg.de>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE."
+},
+
+{
+    "location": "poincare_invariants/#",
+    "page": "Poincaré Invariants",
+    "title": "Poincaré Invariants",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "poincare_invariants/#Poincaré-Integral-Invariants-1",
+    "page": "Poincaré Invariants",
+    "title": "Poincaré Integral Invariants",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "poincare_invariant_1st/#",
+    "page": "1st Poincaré Invariant",
+    "title": "1st Poincaré Invariant",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "poincare_invariant_1st/#st-Poincaré-Integral-Invariant-1",
+    "page": "1st Poincaré Invariant",
+    "title": "1st Poincaré Integral Invariant",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "poincare_invariant_2nd/#",
+    "page": "2nd Poincaré Invariant",
+    "title": "2nd Poincaré Invariant",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "poincare_invariant_2nd/#nd-Poincaré-Integral-Invariant-1",
+    "page": "2nd Poincaré Invariant",
+    "title": "2nd Poincaré Integral Invariant",
+    "category": "section",
+    "text": ""
+},
+
+]}
