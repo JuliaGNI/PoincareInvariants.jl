c6dbca4af7d445936b02ce82adad5e8f8f1f08cb
Michael Kraus <michael.kraus@ipp.mpg.de>
2020-09-22 14:50:26 +0200

    Increase verbosity level in tests so that Travis does not kill the test because of time outs.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,8 @@
 
+using GeometricIntegrators
 using SafeTestsets
+
+set_config(:verbosity, 2)
 
 @safetestset "1st Poincaré Invariant Unit Tests                                               " begin include("poincare_invariant_1st_unittests.jl") end
 @safetestset "1st Poincaré Invariant Function Tests                                           " begin include("poincare_invariant_1st_tests.jl") end
