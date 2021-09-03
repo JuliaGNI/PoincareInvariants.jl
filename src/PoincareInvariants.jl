cf88723b223e13b50669c424f03850e590305beb
Paul Nemec <paul.nemec@tum.de>
2021-09-03 14:34:28 +0200

    Add get_phase_points!

--- src/PoincareInvariants.jl
+++ src/PoincareInvariants.jl
@@ -1,4 +1,23 @@
 module PoincareInvariants
 
+# calculates phase points using param_func and outputs a vector of values for each
+# dimension of phase space (as opposed to vector of SVectors)
+#
+# Type annotations added to force specialisation on function
+#
+# TODO: add @generated version with @nexprs
+# TODO: Should generated version also use known point number?
+function get_phase_points!(param_func::F, out, param_points, ::Val{N}) where {F, N}
+    N::Int  # base Julia ntuple.jl does this, too
+    
+    for (i, pa_pnt) in enumerate(param_points)
+        ph_pnt = param_func(pa_pnt)
+        for j in 1:N
+            out[j][i] = ph_pnt[j]
+        end
+    end
+
+    out
+end
 
 end  # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,32 @@
 using Test, PoincareInvariants
 
-@testset "PoincareInvariants.jl" begin
+using Random: MersenneTwister
 
+using StaticArrays
+
+@testset "PoincareInvariants.jl" begin
+    @testset "get_phase_points!" begin
+        point_num = 100
+        T = Float64
+        N = 4
+
+        param_func(v) = SVector{4}(
+            v[1] * v[2],
+            v[1] * 2,
+            v[1] + v[2],
+            v[1] + 2
+        )
+
+        out = ntuple(_ -> zeros(T, point_num), 4)
+
+        param_points = let rng = MersenneTwister(1234)
+            [SVector(rand(rng, T), rand(rng, T)) for _ in 1:point_num]
+        end
+
+        PoincareInvariants.get_phase_points!(param_func, out, param_points, Val(4))
+
+        @test out[begin] == first.(param_func.(param_points))
+
+        @test out[end] == last.(param_func.(param_points))
+    end
 end
