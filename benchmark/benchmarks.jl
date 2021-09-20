4af76778b0d34e9abf12df8cef04be7eb8beeac9
Paul Nemec <paul.nemec@tum.de>
2021-09-20 17:55:14 +0200

    Add benchmarks

---
+++ benchmark/benchmarks.jl
@@ -0,0 +1,17 @@
+using BenchmarkTools, PoincareInvariants
+
+const SUITE = BenchmarkGroup()
+
+SUITE["FirstPoincareInvariants"] = BenchmarkGroup()
+
+SUITE["SecondPoincareInvariants"] = BenchmarkGroup()
+
+for N in [100 * 100, 1000 * 1000], D in [2, 12, 100]
+    name = "SecondPoincareInvariant{$D, Float64}(Ω, $N)"
+    pinv = SecondPoincareInvariant{D, Float64}(CanonicalSymplecticMatrix(D), N)
+    
+    SUITE["SecondPoincareInvariants"]["canonical"][name] =
+        @benchmarkable compute($pinv, phasepoints) setup=begin
+            phasepoints = rand($(pinv.N), $D)
+        end
+end
