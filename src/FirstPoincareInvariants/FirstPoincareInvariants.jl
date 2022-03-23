4ebc4d8d67d0aef0714252905312877e821eb4aa
Paul Nemec <paul.nemec@tum.de>
2022-03-23 14:52:51 +0100

    Starting fresh

--- src/FirstPoincareInvariants/FirstPoincareInvariants.jl
+++ src/FirstPoincareInvariants/FirstPoincareInvariants.jl
@@ -1,9 +1,5 @@
 @reexport module FirstPoincareInvariants
 
-using OffsetArrays
 
-include("poincare_invariant_1st_common.jl")
-include("poincare_invariant_1st.jl")
-include("poincare_invariant_1st_canonical.jl")
 
 end  # module FirstPoincareInvariants
