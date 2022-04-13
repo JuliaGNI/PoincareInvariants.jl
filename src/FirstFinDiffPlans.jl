8d9cbea962f29daee9da07292be54a009c45c31d
Paul Nemec <paul.nemec@tum.de>
2022-04-13 09:46:20 +0200

    Use matrix to represent points for FirstDiffPlan

--- src/FirstFinDiffPlans.jl
+++ src/FirstFinDiffPlans.jl
@@ -19,36 +19,30 @@ function compute!(
     θ = getform(pinv)
     I = zero(T)
 
-    for i in 2:N-1
-        zi = SVector{D}(ntuple(d -> zs[d][i], D))
-        dzi = ntuple(D) do d
-            (zs[d][i+1] - zs[d][i-1]) / 2
-        end |> SVector{D}
+    dzi = Vector{T}(undef, D)
 
+    for i in 2:N-1
+        zi = view(zs, i, :)
+        dzi .= @views (zs[i+1, :] .- zs[i-1, :]) ./ 2
         I += dot(θ(zi, t, p), dzi)
     end
 
     # special cases i = 1
-    z1 = SVector{D}(ntuple(d -> zs[d][1], D))
-    dz1 = ntuple(D) do d
-        (zs[d][2] - zs[d][N]) / 2
-    end |> SVector{D}
-
-    I += dot(θ(z1, t, p), dz1)
+    z1 = view(zs, 1, :)
+    dzi .= @views (zs[2, :] .- zs[N, :]) ./ 2
+    I += dot(θ(z1, t, p), dzi)
 
     # special cases i = N
-    zN = SVector{D}(ntuple(d -> zs[d][N], D))
-    dzN = ntuple(D) do d
-        (zs[d][1] - zs[d][N-1]) / 2
-    end |> SVector{D}
-
-    I += dot(θ(zN, t, p), dzN)
+    zN = view(zs, N, :)
+    dzi .= @views (zs[1, :] .- zs[N-1, :]) ./ 2
+    I += dot(θ(zN, t, p), dzi)
 
     return I
 end
 
 ## getpoints and getpointspec ##
 
+# must be even to work with simpson weights
 function getpointspec(N::Integer, ::Type{<:FirstFinDiffPlan})::Int
     if N < 2
         return 2
@@ -61,17 +55,14 @@ end
 
 function getpoints(f, ::Type{T}, N::Integer, ::Type{<:FirstFinDiffPlan}) where T
     D = length(f(zero(T)))
-    out = ntuple(_ -> Vector{T}(undef, N), D)
+    out = Matrix{T}(undef, N, D)
 
     for (i, x) in enumerate(range(0, 1, length=N+1)[1:end-1])
-        fpnt = f(x)
-        for d in 1:D
-            out[d][i] = fpnt[d]
-        end
+        out[i, :] .= f(x)
     end
 
-    # Should return vector [...] instead of ([...],) in 1D case
-    return D == 1 ? out[1] : out
+    # Should return vector instead of matrix in 1D case
+    return D == 1 ? vec(out) : out
 end
 
-end  # module LoopFinDiff
+end  # module FirstFinDiffPlans
--- test/test_FirstFinDiffPlans.jl
+++ test/test_FirstFinDiffPlans.jl
@@ -5,7 +5,7 @@
     @test getpointspec(50, FirstFinDiffPlan) == 50
     @test getpointspec(345, FirstFinDiffPlan) == 346
 
-    @inferred NTuple{2, Vector{Float32}} getpoints(x -> (x, 2x), Float32, 8, FirstFinDiffPlan)
+    @inferred Matrix{Float32} getpoints(x -> (x, 2x), Float32, 8, FirstFinDiffPlan)
     @inferred Vector{Float64} getpoints(x -> sin(x), Float64, 124, FirstFinDiffPlan)
 
     @test getpoints(identity, Float64, 4, FirstFinDiffPlan) ≈ [0, 0.25, 0.5, 0.75]
@@ -17,14 +17,12 @@
     pnts3 = getpoints(Float32, N, FirstFinDiffPlan) do x
         x, x^2, sin(x)
     end
-    @test pnts3 isa NTuple{3, Vector{Float32}}
-    @test all(pnts3) do v
-        length(v) == N
-    end
+    @test pnts3 isa Matrix{Float32}
+    @test size(pnts3) == (N, 3)
 
-    @test pnts3[1] ≈ testpnts
-    @test pnts3[2] ≈ testpnts .* testpnts
-    @test pnts3[3] ≈ sin.(testpnts)
+    @test pnts3[:, 1] ≈ testpnts
+    @test pnts3[:, 2] ≈ testpnts .* testpnts
+    @test pnts3[:, 3] ≈ sin.(testpnts)
 end
 
 @safetestset "compute! with FirstFinDiffPlan" begin
@@ -45,17 +43,15 @@ end
     Δx = 1 / N
 
     testI = sum(1:6) do i
-        dfi = ntuple(3) do d
-            if i == 1
-                return (fvals[d][2] - fvals[d][6]) / (2 * Δx)
-            elseif i == 6
-                return (fvals[d][1] - fvals[d][5]) / (2 * Δx)
-            else
-                return (fvals[d][i+1] - fvals[d][i-1]) / (2 * Δx)
-            end
-        end |> SVector{3}
+        dfi = if i == 1
+            (fvals[2, :] .- fvals[6, :]) ./ (2 * Δx)
+        elseif i == 6
+            (fvals[1, :] .- fvals[5, :]) ./ (2 * Δx)
+        else
+            (fvals[i+1, :] .- fvals[i-1, :]) ./ (2 * Δx)
+        end
 
-        fi = SVector{3}(ntuple(d -> fvals[d][i], 3))
+        fi = fvals[i, :]
         return Δx * dot(θ(fi, 2, 3), dfi)
     end
 
